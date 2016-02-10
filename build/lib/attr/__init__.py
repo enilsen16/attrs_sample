1b6f1901718f2f75868008f2dc393709058d6e9a
Erik Nilsen <enilsen16@live.com>
2016-02-09 18:31:21 -0800

    add sample package

---
+++ build/lib/attr/__init__.py
@@ -0,0 +1,10 @@
+__title__ = "attrs"
+__description__ = "Attributes without boilerplate."
+__uri__ = "https://attrs.readthedocs.org/"
+
+__author__ = "Erik Nilsen"
+__email__ = ""
+
+__license__ = "MIT"
+__copyright__ = "Copyright (c) 2015 Erik"
+__version__ = "0.0.1"
